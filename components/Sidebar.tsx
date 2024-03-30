fe00a5d81459b1fab34c10045ba9afd32839cd6f
samuelRodriguezA <srodriguezarmesto@gmail.com>
2024-03-30 16:40:37 -0400

    Auth Completed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import Image from 'next/image';
 import { sidebarLinks } from '@/constants'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
@@ -9,9 +9,9 @@ const Sidebar = () => {
     const pathname = usePathname();
   return (
     <section className='sticky ;ect-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
-        <div className='flex flex- flex-col gap-6'>
+        <div className='flex flex-1 flex-col gap-6'>
            {sidebarLinks.map((link) => {
-            const isActive = pathname === link.route || pathname.startsWith(link.route);
+            const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
            return(
             <Link 
             href={link.route} 
@@ -20,7 +20,15 @@ const Sidebar = () => {
                 'bg-blue-1': isActive,
             })}
             >
-            {link.label}
+            <Image
+              src= {link.imgUrl}
+              alt={link.label}
+              width={24}
+              height={24}
+            />
+            <p className='text-lg font-semibold max-lg:hidden'>
+              {link.label}
+            </p>
             </Link>
            )
            })}
